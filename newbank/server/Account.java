9294c3826038fdf38ce5493388a3df4b24c54392
Tom Mac <tom@developwithtom.com>
2021-03-23 23:46:47 +0000

    added account number to accounts class

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -4,14 +4,18 @@ public class Account {
 
   private String accountName;
   private double openingBalance;
+  private int accountNumber;
 
-  public Account(String accountName, double openingBalance) {
+  public Account(String accountName, double openingBalance, int accountNumber) {
     this.accountName = accountName;
     this.openingBalance = openingBalance;
+    this.accountNumber = accountNumber;
+    this.sortCode = sortCode;
+
   }
 
   public String toString() {
-    return (accountName + ": " + openingBalance);
+    return (accountName + " (Account No:" + accountNumber + ", " + "Sort Code: " + sortCode + "): " + openingBalance);
   }
 
 }
